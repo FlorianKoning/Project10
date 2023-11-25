0c7c6684c042c5cc324f60c649a78d8fc2db6d86
FlorianKoning <florian.koning2004@gmail.com>
2023-11-25 14:18:44 +0100

    Navbar - Homepagina

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="includes/css/main.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Gemeetne Rotterdam Klachten | Home</title>
+</head>
+<body>
+    <!-- navbar en header img van website -->
+    <header>
+    <!-- navbar met foto -->
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <img src="includes/images/logo-gemeente-rotterdam.png" alt="Logo" width="100" height="30" class="d-inline-block align-text-top">
+            <a class="navbar-brand" style="margin-left: 20px;">Gemeente Rotterdam</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Log in</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+    </header>
+
+    <!-- al het andere van de website -->
+    <main>
+
+
+
+    </main>
+
+    <!-- footer van de website -->
+    <footer>
+
+
+
+    </footer>
+</body>
+</html>
