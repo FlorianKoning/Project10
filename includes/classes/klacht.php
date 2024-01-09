dfc4229a87b2fdd0c3ace3c88d46fd6b44ffdd4f
Valentijn10 <102591861+Valentijn10@users.noreply.github.com>
2024-01-09 13:01:55 +0100

    Read klacht

---
+++ includes/classes/klacht.php
@@ -0,0 +1,65 @@
+<?php
+//* florian koning
+//! niet vergeten om een session te hebben om het accountID te krijgen
+
+require_once 'connect.php';
+
+class klacht
+{
+    private $conn;
+
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
+
+    // *create klacht
+    public function create(string $titel, string $omschrijving, string $tijdstip, string $coordinaten, int $accountID)
+    {
+        //* de sql code en de prepare om een nieuwe klacht te maken
+        $sql = "INSERT INTO klacht (Titel, Omschrijving, Tijdstip, Coordinaten, accountID) VALUES (?, ?, ?, ?, ?)";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([$titel, $omschrijving, $tijdstip, $coordinaten, $accountID]);
+
+        //* checkt of $stmt is executed en laat dan weten of de klacht is aangemaakt of niet
+        if ($stmt) {
+            echo "<p style='color: white;'>Gelukt om Nieuwe klacht in te dienen</p>";
+        } else {
+            echo "<p>Er is iets fout gegaan, kon geen nieuwe klacht aanmaken!</p>";
+        }
+    }
+
+    // *update klacht
+    public function update(string $titel, string $omschrijving, string $tijdstip, string $coordinaten, $ID)
+    {
+        //* de sql code en de prepare om een klacht te updaten
+        $sql = "UPDATE klacht SET Titel = ?, Omschrijving = ?, Tijdstip = ?, Coordinaten = ? WHERE ID = ?";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([$titel, $omschrijving, $tijdstip, $coordinaten, $ID]);
+
+        //* checkt of $stmt is executed en laat dan weten of de klacht is geupdate of niet
+        if ($stmt) {
+            echo 'Je klacht is bij gewerkt';
+        } else {
+            echo "Er is iets fout gegaan, kon de klacht niet bijwerken!";
+        }
+    }
+
+    // * Read klacht
+    public function readKlacht()
+    {
+        $db = new Database("localhost","root","","project_10");
+
+        // Alle klacht gegevens in de database
+        $klachtlijst = $db->SQLCommando("select * from klacht where 1",[]);
+
+        foreach($klachtlijst as $klacht){
+            echo "<p>ID: " . $klacht["ID"] . "<br>";
+            echo "Titel: " . $klacht["Titel"] . "<br>";
+            echo "Omschrijving: " . $tocht["Omschrijving"] . "<br>";
+            echo "Tijdstip: " . $tocht["Tijdstip"] . "<br>";
+            echo "Coordinaten: " . $tocht["Coordinaten"] . "<br>";
+            echo "klantID: " . $tocht["klantID"] . "<br></p>";
+        }
+    }
+}
