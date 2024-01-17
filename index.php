65326c66d3ed5af50c252aabb591956ba0c27244
Vincent Kroon <94542221+VincentKroon@users.noreply.github.com>
2024-01-17 10:51:09 +0100

    session clear
    clear email session

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
-$_SESSION['email'] = "";
-$_SESSION['ingelogt'] = false;
+$_SESSION['email'] = NULL;
 
 require_once './includes/classes/account.php';
 require_once './includes/classes/klacht.php';
